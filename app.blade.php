415afd63dd7be87d059fd93e8ea570650258049f
kosukezai <kosukexyz2525@ezweb.ne.jp>
2023-04-04 18:40:15 +0900

    白紙にして一から

---
+++ app.blade.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+       <meta charser="UTF-8">
+        <!-- CSRF Token -->
+       <meta name="csrf-token" content="{{ csrf_token() }}">
+
+       <title>メモ帳</title>
+       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+       
+     </head>
+
+    <body>
+       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+  <h1 class="navbar-brand">Navbar</h1><br>
+  </div>
+  </div>
+      <div class="container">
+  <div class="row">
+
+    <div class="col">
+<div class="card">
+  <div class="card-body">
+  <div style="padding-bottom:800px">
+     Column
+  </div>
+    </div>
+  </div>
+  </div>
+    <div class="col">
+<div class="card">
+  <div class="card-body">
+     Column
+    </div>
+</div>
+</div>
+    <div class="col">
+<div class="card">
+  <div class="card-body">
+      Column
+    </div>
+</div>
+</div>
+  </div>
+</div>
+        
+       
+    </body>
+</html>
