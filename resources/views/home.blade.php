6687c606751055f992a9e2d739496a9b40639c9f
kosukezai <kosukexyz2525@ezweb.ne.jp>
2023-04-11 13:07:13 +0900

    formに入力した内容をデータベースに格納できるようになった。

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,9 +9,11 @@
   </div>
   <div class="card-body">
   <div style="padding-bottom:600px;">
-  <form method='POST' action="/store">
-  
+  <form method='POST' action="laravelapp/public/store">
+  @csrf
+  <input type='hidden' name='user_id' value="{{ $user['id'] }}">
   <textarea name='content' class="form-control"></textarea>
+  <button type='submit' class="btn btn-primary btn-lg">保存</button>
   </form>
   </div>
   </div>
